74da518bcbc7ba1b831f74d7d4ec052779abac08
Israel de Oliveira <saymons47@hotmail.com>
2024-06-11 20:39:25 -0300

    adiciona o model da task

---
+++ models/task.py
@@ -0,0 +1,16 @@
+class Task:
+  def __init__(self, id, title, description, done=False) -> None:
+    self.id = id
+    self.title = title
+    self.description = description
+    self.done = done
+  
+  def to_dict(self):
+    return {
+      "id": self.id,
+      "title": self.title,
+      "description": self.description,
+      "done": self.done
+    }
+
+
